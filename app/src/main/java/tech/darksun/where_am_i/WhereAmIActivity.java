1792db9759f97149c7c57359587db1e42b73fa9f
Hans Christian Nenseth <nenseth@gmail.com>
2018-05-15 14:52:03 +0200

    dir push

---
+++ app/src/main/java/tech/darksun/where_am_i/WhereAmIActivity.java
@@ -0,0 +1,13 @@
+package tech.darksun.where_am_i;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhereAmIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_where_am_i);
+    }
+}
